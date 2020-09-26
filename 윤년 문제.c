003bcc900b4b4a7940d70a87980628ff2e8a88f6
YeongSan-Kim-03 <65652613+YeongSan-Kim-03@users.noreply.github.com>
2020-09-26 20:15:04 +0900

    백준 알고리즘 윤년 문제
    값을 입력받아 그값이 윤년인지(4년마다, 400년 마다 // 100년은 평년임) 평년인지 판단하는 문제이다.

--- 윤년 문제.c
+++ 윤년 문제.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
-int main(){
+int main() {
 	int Year;
 	scanf_s("%d", &Year);
+	
+	if (Year >= 1&&Year <=4000&&Year % 4 == 0 && Year % 100 !=0 || Year % 400 == 0)
+	{
+		printf("1");
+	}
+	else 
+	{
+		printf("0");
+	}
 
-	if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
-	{
-		printf("윤년");
-	}
-	else
-	{
-		printf("평년");
-	}
+
+
 	return 0;
 }
